3b14b4b075ad2be4a3c6c121656f84f661b0e089
Manikanta-04 <238w1a12a7@vrsec.ac.in>
2026-04-17 14:01:59 +0530

    completed the add to cart section

--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,31 @@
-const express = require("express")
-const mongoose = require("mongoose")
+const express= require("express")
+const mongoose= require("mongoose")
+const app= express()
 const cors = require("cors")
 require("dotenv").config()
-
-const app = express()
-
-const authRoutes = require("./routes/authRoutes")
-const productRoutes = require("./routes/productRoutes") // ✅ ADD THIS
+const authRoutes=require("./routes/authRoutes")
+const productRoutes=require("./routes/productRoutes")
 
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-
-// ROUTES
-app.use("/api/auth", authRoutes)
-app.use("/api/product", productRoutes)
-
-// TEST ROUTE
-app.get("/", (req, res) => {
-    res.json({ message: "server is running" })
-})
-
-// DB CONNECTION
+app.use(express.urlencoded({
+    extended:true
+}))
 mongoose.connect(process.env.MONGODB_URL)
-    .then(() => {
+    .then(()=>{
         console.log("DB connected")
-
-        app.listen(5000, () => {
-            console.log("server runs on port 5000")
-        })
     })
-    .catch((err) => {
+    .catch((err)=>{
         console.log(err)
     })
+
+app.get("/",(req,res)=>{
+    res.json({message:"server is running"})
+})
+app.use("/api/auth",authRoutes)
+app.use("/api/product",productRoutes)
+
+const cartRoutes=require("./routes/cartRoutes")
+app.use("/api/cart",cartRoutes)
+
+app.listen(5000,()=>console.log("server runns on port 5000"))
